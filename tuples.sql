afa8a1e9fb39a45e0d103b3d0129ecd42d876407
Lucas <lucas.zamprogno@gmail.com>
2016-10-28 14:41:02 -0700

    Adding more tuples

--- tuples.sql
+++ tuples.sql
@@ -1,20 +1,41 @@
-insert into faculty values (242518,'James Smith','Vancouver');
-insert into faculty values (141582,'Mary Johnson','Vancouver');
-insert into faculty values (011564,'John Williams','Vancouver');
-insert into faculty values (254099,'Patricia Jones','Montreal');
-insert into faculty values (090873,'Elizabeth Taylor','Montreal');
-insert into faculty values (356187,'Robert Brown','Edmonton');
-insert into faculty values (489456,'Linda Davis','Edmonton');
-insert into faculty values (287321,'Michael Miller','Toronto');
-insert into faculty values (248965,'Barbara Wilson','Toronto');
-insert into faculty values (159542,'William Moore','Toronto');
-insert into faculty values (486512,'David Anderson','Victoria');
-insert into faculty values (619023,'Jennifer Thomas','Ottowa');
-insert into faculty values (489221,'Richard Jackson','Winnipeg');
-insert into faculty values (548977,'Ulysses Teach','Regina');
+insert into health_care_provider values (242518,'James Smith','Vancouver'); --
+insert into health_care_provider values (141582,'Mary Johnson','Vancouver');
+insert into health_care_provider values (011564,'John Williams','Vancouver'); --
+insert into health_care_provider values (254099,'Patricia Jones','Montreal'); --
+insert into health_care_provider values (090873,'Elizabeth Taylor','Montreal');
+insert into health_care_provider values (356187,'Robert Brown','Edmonton');
+insert into health_care_provider values (489456,'Linda Davis','Edmonton'); --
+insert into health_care_provider values (287321,'Michael Miller','Toronto');
+insert into health_care_provider values (248965,'Barbara Wilson','Toronto'); --
+insert into health_care_provider values (159542,'William Moore','Toronto');
+insert into health_care_provider values (486512,'David Anderson','Victoria'); --
+insert into health_care_provider values (619023,'Jennifer Thomas','Ottowa'); --
+insert into health_care_provider values (489221,'Richard Jackson','Winnipeg'); --
+insert into health_care_provider values (548977,'Ulysses Teach','Regina'); --
+insert into health_care_provider values (487552,'Barak Jacob','Victoria');
+insert into health_care_provider values (334051,'Lucas Zamprogno','Ottowa');
+insert into health_care_provider values (472122,'Vanessa Aeschbach','Regina');
+insert into health_care_provider values (163784,'Varoon Mathur','Winnipeg');
 
--- FAMILY PHYSICIAN
--- SPECIALIST
+insert into family_physician values (242518,'James Smith','Vancouver'); --
+insert into family_physician values (011564,'John Williams','Vancouver'); --
+insert into family_physician values (254099,'Patricia Jones','Montreal'); --
+insert into family_physician values (489456,'Linda Davis','Edmonton'); --
+insert into family_physician values (248965,'Barbara Wilson','Toronto'); --
+insert into family_physician values (486512,'David Anderson','Victoria'); --
+insert into family_physician values (619023,'Jennifer Thomas','Ottowa'); --
+insert into family_physician values (489221,'Richard Jackson','Winnipeg'); --
+insert into family_physician values (548977,'Ulysses Teach','Regina');
+
+insert into specialist values (141582,'Mary Johnson','Vancouver');
+insert into specialist values (090873,'Elizabeth Taylor','Montreal');
+insert into specialist values (356187,'Robert Brown','Edmonton');
+insert into specialist values (287321,'Michael Miller','Toronto');
+insert into specialist values (159542,'William Moore','Toronto');
+insert into specialist values (487552,'Barak Jacob','Victoria');
+insert into specialist values (334051,'Lucas Zamprogno','Ottowa');
+insert into specialist values (472122,'Vanessa Aeschbach','Regina');
+insert into specialist values (163784,'Varoon Mathur','Winnipeg');
 
 insert into patient_registered values(060839453,'Charles Harris','Vancouver');
 insert into patient_registered values(099354543,'Susan Martin','Montreal');
@@ -45,44 +66,61 @@ insert into patient_registered values(578875478,'Edward Baker','Ottowa');
 -- IS ON
 insert into waitlist values('Vancouer', 'Cardiology');
 insert into waitlist values('Vancouer', 'Oncology');
-insert into waitlist values('Vancouer', 'Pediatry');
+insert into waitlist values('Vancouer', 'Podiatry');
 insert into waitlist values('Vancouer', 'Radiology');
 insert into waitlist values('Vancouer', 'Surgery');
 insert into waitlist values('Edmonton', 'Cardiology');
 insert into waitlist values('Edmonton', 'Oncology');
-insert into waitlist values('Edmonton', 'Pediatry');
+insert into waitlist values('Edmonton', 'Podiatry');
 insert into waitlist values('Edmonton', 'Radiology');
 insert into waitlist values('Edmonton', 'Surgery');
 insert into waitlist values('Regina', 'Cardiology');
 insert into waitlist values('Regina', 'Oncology');
-insert into waitlist values('Regina', 'Pediatry');
+insert into waitlist values('Regina', 'Podiatry');
 insert into waitlist values('Regina', 'Radiology');
 insert into waitlist values('Regina', 'Surgery');
 insert into waitlist values('Winnipeg', 'Cardiology');
 insert into waitlist values('Winnipeg', 'Oncology');
-insert into waitlist values('Winnipeg', 'Pediatry');
+insert into waitlist values('Winnipeg', 'Podiatry');
 insert into waitlist values('Winnipeg', 'Radiology');
 insert into waitlist values('Winnipeg', 'Surgery');
 insert into waitlist values('Toronto', 'Cardiology');
 insert into waitlist values('Toronto', 'Oncology');
-insert into waitlist values('Toronto', 'Pediatry');
+insert into waitlist values('Toronto', 'Podiatry');
 insert into waitlist values('Toronto', 'Radiology');
 insert into waitlist values('Toronto', 'Surgery');
 insert into waitlist values('Montreal', 'Cardiology');
 insert into waitlist values('Montreal', 'Oncology');
-insert into waitlist values('Montreal', 'Pediatry');
+insert into waitlist values('Montreal', 'Podiatry');
 insert into waitlist values('Montreal', 'Radiology');
 insert into waitlist values('Montreal', 'Surgery');
 insert into waitlist values('Victoria', 'Cardiology');
 insert into waitlist values('Victoria', 'Oncology');
-insert into waitlist values('Victoria', 'Pediatry');
+insert into waitlist values('Victoria', 'Podiatry');
 insert into waitlist values('Victoria', 'Radiology');
 insert into waitlist values('Victoria', 'Surgery');
 insert into waitlist values('Ottowa', 'Cardiology');
 insert into waitlist values('Ottowa', 'Oncology');
-insert into waitlist values('Ottowa', 'Pediatry');
+insert into waitlist values('Ottowa', 'Podiatry');
 insert into waitlist values('Ottowa', 'Radiology');
 insert into waitlist values('Ottowa', 'Surgery');
+
 -- TAKES
--- MEDICATION
+
+insert into medication values ('Morpine', 80);
+insert into medication values ('Statin', 80);
+insert into medication values ('Abraxane', 80);
+insert into medication values ('luliconazole', 80);
+insert into medication values ('Gravol', 80);
+insert into medication values ('Morpine', 100);
+insert into medication values ('Statin', 100);
+insert into medication values ('Abraxane', 100);
+insert into medication values ('luliconazole', 100);
+insert into medication values ('Gravol', 100);
+insert into medication values ('Morpine', 120);
+insert into medication values ('Statin', 120);
+insert into medication values ('Abraxane', 120);
+insert into medication values ('luliconazole', 120);
+insert into medication values ('Gravol', 120);
+
 -- PRESCRIBE
