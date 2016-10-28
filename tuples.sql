cb4923e1f0b95401400e2953cd18b336deeacf91
Lucas <lucas.zamprogno@gmail.com>
2016-10-28 14:08:27 -0700

    Tuples for all tables without foreign keys

---
+++ tuples.sql
@@ -0,0 +1,88 @@
+insert into faculty values (242518,'James Smith','Vancouver');
+insert into faculty values (141582,'Mary Johnson','Vancouver');
+insert into faculty values (011564,'John Williams','Vancouver');
+insert into faculty values (254099,'Patricia Jones','Montreal');
+insert into faculty values (090873,'Elizabeth Taylor','Montreal');
+insert into faculty values (356187,'Robert Brown','Edmonton');
+insert into faculty values (489456,'Linda Davis','Edmonton');
+insert into faculty values (287321,'Michael Miller','Toronto');
+insert into faculty values (248965,'Barbara Wilson','Toronto');
+insert into faculty values (159542,'William Moore','Toronto');
+insert into faculty values (486512,'David Anderson','Victoria');
+insert into faculty values (619023,'Jennifer Thomas','Ottowa');
+insert into faculty values (489221,'Richard Jackson','Winnipeg');
+insert into faculty values (548977,'Ulysses Teach','Regina');
+
+-- FAMILY PHYSICIAN
+-- SPECIALIST
+
+insert into patient_registered values(060839453,'Charles Harris','Vancouver');
+insert into patient_registered values(099354543,'Susan Martin','Montreal');
+insert into patient_registered values(112348546,'Joseph Thompson','Vancouver'));
+insert into patient_registered values(115987938,'Christopher Garcia','Edmonton');
+insert into patient_registered values(132977562,'Angela Martinez','Victoria');
+insert into patient_registered values(269734834,'Thomas Robinson','Toronto');
+insert into patient_registered values(280158572,'Margaret Clark','Toronto');
+insert into patient_registered values(301221823,'Juan Rodriguez','Regina');
+insert into patient_registered values(318548912,'Dorthy Lewis','Montreal');
+insert into patient_registered values(320874981,'Daniel Lee','Vancouver'));
+insert into patient_registered values(322654189,'Lisa Walker','Vancouver'));
+insert into patient_registered values(348121549,'Paul Hall','Winnipeg');
+insert into patient_registered values(351565322,'Nancy Allen','Vancouver'));
+insert into patient_registered values(451519864,'Mark Young','Ottowa');
+insert into patient_registered values(455798411,'Luis Hernandez','Edmonton');
+insert into patient_registered values(462156489,'Donald King','Victoria');
+insert into patient_registered values(550156548,'George Wright','Toronto');
+insert into patient_registered values(552455318,'Ana Lopez','Edmonton');
+insert into patient_registered values(556784565,'Kenneth Hill','Winnipeg');
+insert into patient_registered values(567354612,'Karen Scott','Montreal');
+insert into patient_registered values(573284895,'Steven Green','Winnipeg');
+insert into patient_registered values(574489456,'Betty Adams','Vancouver'));
+insert into patient_registered values(578875478,'Edward Baker','Ottowa');
+
+-- HEALTH CARE RECORD
+-- HAS APPOINTMENT
+-- IS ON
+insert into waitlist values('Vancouer', 'Cardiology');
+insert into waitlist values('Vancouer', 'Oncology');
+insert into waitlist values('Vancouer', 'Pediatry');
+insert into waitlist values('Vancouer', 'Radiology');
+insert into waitlist values('Vancouer', 'Surgery');
+insert into waitlist values('Edmonton', 'Cardiology');
+insert into waitlist values('Edmonton', 'Oncology');
+insert into waitlist values('Edmonton', 'Pediatry');
+insert into waitlist values('Edmonton', 'Radiology');
+insert into waitlist values('Edmonton', 'Surgery');
+insert into waitlist values('Regina', 'Cardiology');
+insert into waitlist values('Regina', 'Oncology');
+insert into waitlist values('Regina', 'Pediatry');
+insert into waitlist values('Regina', 'Radiology');
+insert into waitlist values('Regina', 'Surgery');
+insert into waitlist values('Winnipeg', 'Cardiology');
+insert into waitlist values('Winnipeg', 'Oncology');
+insert into waitlist values('Winnipeg', 'Pediatry');
+insert into waitlist values('Winnipeg', 'Radiology');
+insert into waitlist values('Winnipeg', 'Surgery');
+insert into waitlist values('Toronto', 'Cardiology');
+insert into waitlist values('Toronto', 'Oncology');
+insert into waitlist values('Toronto', 'Pediatry');
+insert into waitlist values('Toronto', 'Radiology');
+insert into waitlist values('Toronto', 'Surgery');
+insert into waitlist values('Montreal', 'Cardiology');
+insert into waitlist values('Montreal', 'Oncology');
+insert into waitlist values('Montreal', 'Pediatry');
+insert into waitlist values('Montreal', 'Radiology');
+insert into waitlist values('Montreal', 'Surgery');
+insert into waitlist values('Victoria', 'Cardiology');
+insert into waitlist values('Victoria', 'Oncology');
+insert into waitlist values('Victoria', 'Pediatry');
+insert into waitlist values('Victoria', 'Radiology');
+insert into waitlist values('Victoria', 'Surgery');
+insert into waitlist values('Ottowa', 'Cardiology');
+insert into waitlist values('Ottowa', 'Oncology');
+insert into waitlist values('Ottowa', 'Pediatry');
+insert into waitlist values('Ottowa', 'Radiology');
+insert into waitlist values('Ottowa', 'Surgery');
+-- TAKES
+-- MEDICATION
+-- PRESCRIBE
