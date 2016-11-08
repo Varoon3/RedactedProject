d844b29eb48d9676ef223c0998b57c44b8aa6b83
Barak Jacob <barakjacob2007@gmail.com>
2016-11-08 00:45:57 -0800

    HCR on mouse over

--- fp_view_two.php
+++ fp_view_two.php
@@ -35,7 +35,6 @@ ul {
 	float:right;
 }
 
-
 table {
 		font-family: 'Arial';
 		margin: 25px auto;
@@ -66,11 +65,29 @@ table {
 		border-right: none;
 	}
 
+.description {
+position:absolute;
+    left: 362px;
+    display:none;
+    border:1px solid #000;
+    width:555px;
+    height:275px;
+}
+
+.title{
+	font-weight: bold;
+	text-decoration: underline;
+}
+
+
+
+
 </style>
 <body>
 
 <p>
 <hr>
+
 <?php
 echo "<ul>";
 echo "<li class = \"item\"><a href=\"index.php?tbl=" .$_GET["tbl"]. "\">My Appointments</a></li>";
@@ -151,15 +168,98 @@ function validateResult($result, $resultNext) { //Checks if the Query is Empty, 
 }
 
 function printAllMyPatients($result){
+	$arr = array();
 	echo "<table>";
 	echo "<thead>";    
 	echo "<tr><th>Care Card Number</th><th>Name</th><th>Location</th></tr></thead>";  
 	echo "<tbody>";
-
+	$count = 0;
 	while ($row = OCI_Fetch_Array($result, OCI_BOTH)){
-		echo "<tr><td><a href=\"fp_view_one.php?carenum=" .$row["CARECARDNUM"]. "\">" . $row["CARECARDNUM"] . "</a></td><td>" . $row["NAME"] . "</td><td>" . $row["LOCATION"] . "</td></tr>"; 
+		echo "<tr id=\"try" .$count. "\" class=\"trying\"><td>" . $row["CARECARDNUM"] . "</td><td>" . $row["NAME"] . "</td><td>" . $row["LOCATION"] . "</td></tr>";
+		$arr[$count] = $row["CARECARDNUM"];
+		$count++;		
 	}
-	
+		sendCountToJs($count);
     echo "</tbody>";
     echo "</table>";	
+	makeHCRBox($arr);
+	
+	
 }
+
+function makeHCRBox($arr){
+	for($i=0;$i<count($arr);$i++){
+		$resultQuery = executePlainSQL("select p.name, p.location, h.carecardnum, h.rid, h.age, h.ethnicity, h.genetichistory, h.insurance from health_care_record h, patient_registered p where h.carecardnum=p.carecardNum and p.carecardnum=$arr[$i]");
+		printHCR($resultQuery,$i);
+	}
+}
+	
+	
+function printHCR($result,$count){
+echo "<div id=\"des" .$count. "\" class = \"description\" >";
+
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+	    echo "<p class = \"title\">Health Care Record for patient " .$row["NAME"]."</p>";
+		echo "Care Card #: " .$row["CARECARDNUM"]. "";
+		echo "<br>";
+		echo "Location: " .$row["LOCATION"]. "";
+		echo "<br>";
+		echo "Record ID: " .$row["RID"]. "";
+		echo "<br>";
+		echo "Age: " .$row["AGE"]. "";
+		echo "<br>";
+		echo "Ethnicity: " .$row["ETHNICITY"]. "";
+		echo "<br>";
+		echo "Genetic History: " .printGeneticHistory($row["GENETICHISTORY"]). "";
+		echo "<br>";
+	}
+echo "</div>";
+}
+
+function sendCountToJs($count){
+	echo "<div id=\"dom-target\" style=\"display: none;\">";
+    echo htmlspecialchars($count); /* You have to escape because the result
+                                           will not be valid HTML otherwise. */
+   echo "</div>";
+}
+
+function printGeneticHistory($gh){
+	$str = "";
+	$strlen = strlen($gh);
+	for($i=0;$i<$strlen;$i++){
+		if($i%50 == 0){
+			$str .= "<br>";
+		}
+		
+		$char = substr( $gh, $i, 1 );
+		$str .= $char;
+		
+	}
+	return $str;
+}
+	
+	?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+
+ var div = document.getElementById("dom-target");
+var count = div.textContent;
+
+
+
+$("#try0").mouseover(function() {
+    $("#des0").show();
+}).mouseout(function() {
+    $("#des0").hide();
+});
+$("#try1").mouseover(function() {
+    $("#des1").show();
+}).mouseout(function() {
+    $("#des1").hide();
+});
+
+
+</script>
+</body>
+</head>
+</html>
