b1ea934e61fa992287db9a0ca1141c3f37047c82
Lucas Zamprogno <lucas.zamprogno@gmail.com>
2016-11-12 18:22:05 -0800

    Last tests, added a thing or two, formatting

--- query_skeletons.sql
+++ query_skeletons.sql
@@ -1,30 +1,27 @@
--- Things from the functionality doc
 -- HCP
 	-- Get patient HCR
 	SELECT *
 	FROM health_care_record
-	WHERE carecardNum = ENTER_HERE
+	WHERE carecardNum = CCN_HERE
 	ORDER BY carecardNum;
 
 	-- Update patient medical history
 	UPDATE health_care_record
 	SET -- We need to decide what's being updated...
-	WHERE carecardNum = ENTER_HERE;
+	WHERE carecardNum = CCN_HERE;
 
 	-- Create appointment
-	insert into has_appointment values (PID_HERE, HID_HERE,'DATE_HERE','TIME_HERE');
+	insert into has_appointment values (CCN_HERE, HID_HERE,'DATE_HERE','TIME_HERE');
 
 	-- Remove appointment
 	DELETE FROM has_appointment
 	WHERE carecardNum = CCN_HERE AND hid = HID_HERE;
 
-	--* Find all patients with related genome substring
+	-- Find all patients with related genome substring
 	SELECT p.name, p.carecardNum
 	FROM patient_registered p, health_care_record h
 	WHERE p.carecardNum = h.carecardNum AND h.geneticHistory LIKE '%SUBSTRING_HERE%'
 	ORDER BY p.carecardNum;
-
-	--* Personalalized health care report (don't understand)
 
 -- Family physician
 	-- Get all patients registered with
@@ -36,27 +33,27 @@
 	-- Add patient to waitlist
 	UPDATE is_on
 	SET patientPriorityNum = patientPriorityNum + 1
-	WHERE patientPriorityNum >= PRIORITY_HERE AND region = REGION_HERE AND speciality = SPEC_HERE;
+	WHERE patientPriorityNum >= PRIORITY_HERE AND region = 'REGION_HERE' AND speciality = 'SPEC_HERE';
 		-- then do 
 	insert into is_on values(CCN_HERE, 'REGION_HERE', 'SPEC_HERE', PRIORITY_HERE, 'DATE_HERE', 'TIME_HERE');
 
-	-- Register with patient (I added this)
+	-- Register with patient
 	UPDATE patient_registered
 	SET hid = HID_HERE
-	WHERE carecardNum = PID_HERE;
+	WHERE carecardNum = CCN_HERE;
 
 
 -- Specialist
-	--* Waitlist removal (may want to combine this with add appointment?)
-	-- Deletion of any Patient
+	-- Deletion of specific Patient
 	DELETE FROM is_on
 	WHERE carecardNum = CCN_HERE AND region = 'REGION_HERE' AND speciality = "SPEC_HERE";
 		-- then do
 	UPDATE is_on
 	SET patientPriorityNum = patientPriorityNum - 1
-	WHERE patientPriorityNum >= PRIORITY_HERE AND region = REGION_HERE AND speciality = SPEC_HERE;
+	WHERE patientPriorityNum >= PRIORITY_HERE AND region = 'REGION_HERE' AND speciality = 'SPEC_HERE';
 
-	-- Pull top priority patient from waitlist
+	-- Take the top priority patient off a waitlist
+		-- Pull top priority patient from waitlist (to make an appointment with or something)
 	SELECT p.carecardNum, p.name
 	FROM patient p, is_on w -- w for "waitlist"
 	WHERE p.carecardNum = w.carecardNum AND w.patientPriorityNum = 1 AND w.region = 'REGION_HERE' AND w.speciality = 'SPEC_HERE';
@@ -66,15 +63,20 @@
 		-- then do
 	UPDATE is_on
 	SET patientPriorityNum = patientPriorityNum - 1
-	WHERE patientPriorityNum >= 1 AND region = REGION_HERE AND speciality = SPEC_HERE;
+	WHERE patientPriorityNum >= 1 AND region = 'REGION_HERE' AND speciality = 'SPEC_HERE';
 
 	-- Create prescription
 	insert into prescribes values (HID_HERE, 'DRUG_HERE', DOSE_HERE);
 
-	--* Create new patient and HCR
+	-- Create new patient and HCR
 	insert into patient_registered values(CCN_HERE,'NAME_HERE','LOC_HERE', NULL);
 	insert into health_care_record values(CCN_HERE, RID_HERE, 0, 'ETH_HERE', 'INSURANCE_HERE', 'GENOME_HERE');
-		-- Could we automate the genome? That would be sweet. Consider retrieving RID automatically
+		-- Could we automate the genome? That would be sweet.
+
+	-- Get the next RID to use
+	SELECT MAX(RID) + 1 as nextRID
+	FROM (SELECT RID FROM health_care_record);
+
 
 -- Patient
 	-- View medical history
@@ -82,7 +84,6 @@
 	FROM health_care_record
 	WHERE carecardNum = ENTER_HERE;
 
-	-- Address change (redacted?)
 	-- Retreive registered physician
 	SELECT f.name
 	FROM patient_registered p, family_physician f
@@ -93,22 +94,12 @@
 	FROM takes
 	WHERE carecardNum = CCN_HERE;
 
-	--* Stuff about compliance?
-
--- From Varoon pics
--- 1)
-	-- Get HCR (done above)
-	-- Select attributes of patient (too varied for here)
 	-- Get Priority # for patient on waitlist
 	SELECT patientPriorityNum
 	FROM is_on
 	WHERE carecardNum = CCN_HERE AND region = 'REGION_HERE' AND speciality = 'SPEC_HERE';
 
--- 2)
-	-- My appointments based on location (not sure how)
-	-- Health care records grab (?)
--- 3)
-	-- Find all patients on all waitlist (this will never happen lol)
+-- Aggregate - general
 	-- Find all patients taking each type of medications (will need to change tuples so someone does)
 	SELECT p.name, p.carecardNum
 	FROM patient_registered p
@@ -118,7 +109,6 @@
 	 FROM medication m, takes t
 	 WHERE t.carecardNum = p.carecardNum AND m.medName = t.medName));
 
--- 4)
 	-- Find medication with lowest dose
 	SELECT medName, MAX(dose)
 	FROM medication
@@ -131,7 +121,6 @@
 	GROUP BY medName
 	HAVING MIN(dose) = (SELECT MIN(dose) FROM medication);
 
--- 5)
 	-- Number of patients by ethnicity
 	SELECT ethnicity, COUNT(*)
 	FROM health_care_record
@@ -143,11 +132,3 @@
 	FROM health_care_record
 	GROUP BY ethnicity
 	ORDER BY AVG(age);
-
--- 6)
-	
-
-	
-
--- 7)
-	-- Update a very large priority #, do it correctly again (not sure what this means)
